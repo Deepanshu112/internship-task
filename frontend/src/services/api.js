5b4e11ed00a3bbc61dd93f59e99aa10600f89e7c
Deepanshu Varshney <140751260+Deepanshu112@users.noreply.github.com>
2026-01-22 01:13:12 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: ""https://internship-task-zwdl.onrender.com/api"",
+  baseURL: "https://internship-task-zwdl.onrender.com/api",
 });
 
 export default api;
